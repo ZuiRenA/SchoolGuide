33a0921396e298bf597e0dd6a22da993ca4bdcad
Null <a13405665741@gmail.com>
2019-02-22 19:53:24 +0800

    feat:注册基础界面

--- app/src/main/java/com/example/schoolguide/login/RegisterActivity.kt
+++ app/src/main/java/com/example/schoolguide/login/RegisterActivity.kt
@@ -2,13 +2,43 @@ package com.example.schoolguide.login
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.View
 import com.example.schoolguide.BaseActivity
 import com.example.schoolguide.R
+import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : BaseActivity() {
+class RegisterActivity : BaseActivity(), View.OnClickListener {
+    private var isShow = false
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.registerPswShow -> {
+                if (isShow) {
+                    isShow = false
+                    registerPswShow.setImageResource(R.drawable.password_unshow)
+                    registerPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                } else {
+                    isShow = true
+                    registerPswShow.setImageResource(R.drawable.password_show)
+                    registerPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                }
+            }
+
+            R.id.registerBtn -> {   }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        initClick()
+    }
+
+    private fun initClick() {
+        registerPswShow.setOnClickListener(this)
+        registerBtn.setOnClickListener(this)
     }
 }
